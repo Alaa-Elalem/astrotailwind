d92a2a2a8d7af85b9b81f10b97ac970123af6a6e
Alaa Elalem <elalem6542@yahoo.com>
2023-07-17 00:49:02 +0200

    tune cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0f3548",
+        secondary: "#f0c986",
+      },
+    },
   },
   plugins: [],
 };
