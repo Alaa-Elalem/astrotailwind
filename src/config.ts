b52a3cbaeaa81403cd9dc7de8302e04a15c7769c
Alaa Elalem <elalem6542@yahoo.com>
2023-07-26 01:51:01 +0200

    header menu2

--- src/config.ts
+++ src/config.ts
@@ -84,10 +84,12 @@ export { default as DefaultSVG } from "./assets/svg/undraw/undraw_my_feed.svg";
 export { default as DefaultImage } from "./assets/undraw/undraw_my_feed.png";
 
 export const NavigationLinks = [
-  { name: "الصفحة الرئيسية", href: "" },
+  { name: "الصفحة الرئيسية", href: "#" },
   { name: "خدماتنا", href: "about" },
   { name: "تواصل معنا", href: "contact" },
   { name: "طرق الدفع", href: "doc/introduction" },
+  { name: " اعمالنا", href: "#" },
+  { name: " نموذج طلب الخدمة", href: "doc/introduction" },
 ];
 
 export const footerLinks = [
